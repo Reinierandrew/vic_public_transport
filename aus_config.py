af92ba52348c639f9935b21565f30c5b3aa545ea
davidj00@icloud.com <davidj00@icloud.com>
2023-01-10 21:27:54 +1100

    work from Tuesday

---
+++ aus_config.py
@@ -0,0 +1,2 @@
+# Auspost API Key
+auspost_key = "Z9Ori0GFgGfqdg0I2gZKeeAw9y4S5eaK"
---
+++ config.py
@@ -0,0 +1,3 @@
+# Geoapify API Key
+geoapify_key = "a781d85d92924b14bc445fdb573abbcb"
+auspost_key = "Z9Ori0GFgGfqdg0I2gZKeeAw9y4S5eaK"
